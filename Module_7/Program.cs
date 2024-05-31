5221ef6c1bb0fed3628f1be10856b2b88253529e
kurilkoivan7@gmail.com <kurilkoivan7@gmail.com>
2024-06-01 01:55:34 +0600

    7.6.2

--- Module_7/Program.cs
+++ Module_7/Program.cs
@@ -1,19 +1,16 @@
 ﻿using System.Runtime.CompilerServices;
 
-class Helper
+class Car <T>
 {
-    int num1 = 3;
-    int num2 = 58;
-    static void Swap(ref int num1,ref int num2) 
-    {
-        int num3 = num1;
-        num2 = num1;
-        num2 = num3;
-        Console.WriteLine(num1); //58
-        Console.WriteLine(num2); //3
-    } 
+    public T Engine;
+}
+class ElectricEngine 
+{
 
 }
+class GasEngine 
+{
 
+}
 
 
